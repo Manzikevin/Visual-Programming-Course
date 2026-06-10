7c0a39f3f13557e61a6b5720a8604697a7f7ef13
Manzi Kevin <manzikevin04@gmail.com>
2026-06-10 18:29:31 +0200

    Initial Commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 50, b = 100;
+        int sum = a + b;
+        Console.WriteLine(sum);
+        
+        String name = "Manzi Kevin";
+        Console.WriteLine($" My name is  {name}");
+    }
+}
