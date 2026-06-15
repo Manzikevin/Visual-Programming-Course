3100197820805a308e84f493b4d101c50f5b8025
Manzi Kevin <manzikevin04@gmail.com>
2026-06-15 11:44:41 +0200

    Console App 5

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter marks for 5 courses.");
+            int sum = 0, average, marks;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Enter Marks for course {i}:");
+                marks = int.Parse(Console.ReadLine());
+
+                if (marks >= 0 && marks <= 100)
+                {
+                    sum += marks;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input (must be between 0 and 100).");
+                }
+            }
+            
+            average = sum / 5;
+
+            Console.WriteLine($"\nTotal Sum: {sum}");
+            Console.WriteLine($"Average: {average}");
+
+            if (sum < 50)
+            {
+                Console.WriteLine("You Failed");
+            }
+            else
+            {
+                Console.WriteLine("You Passed!");
+            }
+        }
+    }
+}
